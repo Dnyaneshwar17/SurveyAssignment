e6ca11915c672da2046942292fe4984f637e0688
Dnyaneshwar17 <42296046+Dnyaneshwar17@users.noreply.github.com>
2018-08-22 18:43:51 +0530

    Done one browser method for get user required browser

--- utilities_config/Browser.py
+++ utilities_config/Browser.py
@@ -5,39 +5,38 @@ import os
 
 class BrowserConfig(object):
 
- def chrome(self):
-     driverLocation = "D:\\Automation\\chromedriver"
-     os.environ["webdriver.chrome.driver"] = driverLocation
-     driver = webdriver.Chrome(driverLocation)
-     driver.get(InputParameter.url)
-     driver.maximize_window()
-     return driver
+      def chrome(self):
+          driverLocation = "D:\\Automation\\chromedriver"
+          os.environ["webdriver.chrome.driver"] = driverLocation
+          driver = webdriver.Chrome(driverLocation)
+          driver.get(InputParameter.url)
+          driver.maximize_window()
+          return driver
 
 
- def firebox(self):
-      driver = webdriver.Firefox()
-      driver.get(InputParameter.url)
-      driver.maximize_window()
-      return driver
+      def firebox(self):
+          driver = webdriver.Firefox()
+          driver.get(InputParameter.url)
+          driver.maximize_window()
+          return driver
 
 
- def ie(self):
-     driverLocation = "D:\\Automation\\IEDriverServer.exe"
-     os.environ["webdriver.ie.driver"] = driverLocation
-     driver = webdriver.Ie(driverLocation)
-     driver.get(InputParameter.url)
-     driver.maximize_window()
-     return driver
+      def ie(self):
+          driverLocation = "D:\\Automation\\IEDriverServer.exe"
+          os.environ["webdriver.ie.driver"] = driverLocation
+          driver = webdriver.Ie(driverLocation)
+          driver.get(InputParameter.url)
+          driver.maximize_window()
+          return driver
 
- def select_browser(self):
-     browser = BrowserConfig.browser
-     if browser == "Chrome":
-        return BrowserConfig.chrome()
-     elif browser == "Firefox":
-         return BrowserConfig.firebox()
-     elif browser == "IE":
-         return BrowserConfig.ie(self)
-
-     else:
-         print("Please Select Proper Browser")
-         return 0
+      def select_browser(self):
+          browser = BrowserConfig.browser
+          if browser == "Chrome":
+            return BrowserConfig.chrome()
+          elif browser == "Firefox":
+           return BrowserConfig.firebox()
+          elif browser == "IE":
+           return BrowserConfig.ie(self)
+          else:
+           print("Please Select Proper Browser")
+           return 0
