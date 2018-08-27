5da9d4ff37007bfbfd1e818d8563e1c0d212c9a6
Dnyaneshwar17 <42296046+Dnyaneshwar17@users.noreply.github.com>
2018-08-27 19:47:31 +0530

    new test cases of question

--- testcasedemo/test_cases_survey.py
+++ testcasedemo/test_cases_survey.py
@@ -43,49 +43,79 @@ def test_survey_operation(select_driver):
     assert result == True
 
 
+@pytest.mark.run(order=4)
+def test_question_first(select_driver):
+    print("first question ")
+    questions_first = AskQuestion(select_driver)
+    questions_first.first_question(first_question_enter)
+    result = questions_first.verify_question()
+    print("Result: " + str(result))
 
-# @pytest.mark.run(order=6)
-# def test_question_2(get_driver):
-#     print("test_question_2 started")
-#     survey_questions = SurveyQuestionPage(get_driver)
-#     survey_questions.add_question_two(question_no_2)
-#     result = survey_questions.verify_question()
-#     print("Result: " + str(result))
-#     assert result == True
+    assert result == True
+
+@pytest.mark.run(order=5)
+def test_question_2(select_driver):
+    print("second question ")
+    questions_second = AskQuestion(select_driver)
+    questions_second.second_question(second_question_enter)
+    result = questions_second.verify_question()
+    print("Result: " + str(result))
+    assert result == True
+
+@pytest.mark.run(order=6)
+def test_question_4(select_driver):
+    print("four question")
+    questions_four = AskQuestion(select_driver)
+    questions_four.four_question(four_question_enter)
+    result = questions_four.verify_question()
+    print("Result: " + str(result))
+    assert result == True
+
+
+@pytest.mark.run(order=7)
+def test_question_10(select_driver):
+    print("ten question")
+    questions_ten = AskQuestion(select_driver)
+    questions_ten.ten_question(ten_question_enter)
+    result = questions_ten.verify_question()
+    print("Result: " + str(result))
+    assert result == True
+
+
+
+@pytest.mark.run(order=8)
+def test_question_3(select_driver):
+    print("third question")
+    questions_third = AskQuestion(select_driver)
+    questions_third.third_question(third_question_enter)
+    result = questions_third.verify_question()
+    print("Result: " + str(result))
+    assert result == True
+
+@pytest.mark.run(order=9)
+def test_question_5(select_driver):
+    print("five question")
+    questions_five = AskQuestion(select_driver)
+    questions_five.five_question(five_question_enter)
+    result = questions_five.verify_question()
+    print("Result: " + str(result))
+    assert result == True
+
+@pytest.mark.run(order=10)
+def test_question_9(select_driver):
+    print(" nine question ")
+    questions_nine = AskQuestion(select_driver)
+    questions_nine.nine_question(nine_question_enter)
+    result = questions_nine.verify_question()
+    print("Result: " + str(result))
+    assert result == True
+
+# @pytest.mark.run(order=10)
+# def test_question_6(select_driver):
+#         print(" six question ")
+#         questions_six = AskQuestion(select_driver)
+#         questions_six.six_question(six_question_enter)
+#         result = questions_six.verify_question()
+#         print("Result: " + str(result))
+#         assert result == True
 #
-# @pytest.mark.run(order=7)
-# def test_question_3(get_driver):
-#     print("test_question_3 started")
-#     survey_questions = SurveyQuestionPage(get_driver)
-#     survey_questions.add_question_three(question_no_3)
-#     result = survey_questions.verify_question()
-#     print("Result: " + str(result))
-#     assert result == True
-#
-# @pytest.mark.run(order=8)
-# def test_question_4(get_driver):
-#     print("test_question_4 started")
-#     survey_questions = SurveyQuestionPage(get_driver)
-#     survey_questions.add_question_four(question_no_4)
-#     result = survey_questions.verify_question()
-#     print("Result: " + str(result))
-#     assert result == True
-#
-# @pytest.mark.run(order=9)
-# def test_question_5(get_driver):
-#     print("test_question_5 started")
-#     survey_questions = SurveyQuestionPage(get_driver)
-#     survey_questions.add_question_five(question_no_5)
-#     result = survey_questions.verify_question()
-#     print("Result: " + str(result))
-#     assert result == True
-#
-
-
-
-
-
-
-
-
-
