998d88f9bb64aef7b45a1a71a098bc498f8f31b4
Dnyaneshwar17 <42296046+Dnyaneshwar17@users.noreply.github.com>
2018-08-28 19:44:32 +0530

    Done pytest with all question

--- pages/AskQuestion.py
+++ pages/AskQuestion.py
@@ -1,4 +1,4 @@
-from base.Basepage import BasePage
+from base.basepage import BasePage
 from utilities_config.Input import*
 import time
 
@@ -42,6 +42,23 @@ class AskQuestion(BasePage):
     six_question_4 = "//table[@id='rows']/tbody[@class='answerSetting singleLine']/tr[7]/td[2]/div/div[1]"
     six_question_5 = "//table[@id='rows']/tbody[@class='answerSetting singleLine']/tr[8]/td[2]/div/div[1]"
 
+    # seven question
+    seven_question_pattern = "//ul[@class='add-q-menu-right']//a[text()='Matrix / Rating Scale']"
+    seven_question_1 = "//table[@id='rows']/tbody[@class='answerSettingMatrix singleLine']/tr[3]/td[1]/div/div[1]"
+    seven_question_2 = "//table[@id='rows']/tbody[@class='answerSettingMatrix singleLine']/tr[4]/td[1]/div/div[1]"
+    seven_question_3 = "//table[@id='rows']/tbody[@class='answerSettingMatrix singleLine']/tr[5]/td[1]/div/div[1]"
+    seven_question_4 = "//div[@id='columnsWrap']//table/tbody[@class='columnSetting singleLine']/tr[2]/td[1]/div/div[1]"
+    seven_question_5 = "//div[@id='columnsWrap']//table/tbody[@class='columnSetting singleLine']/tr[3]/td[1]/div/div[1]"
+    seven_question_6 = "//div[@id='columnsWrap']//table/tbody[@class='columnSetting singleLine']/tr[4]/td[1]/div/div[1]"
+    seven_question_7 = "//div[@id='columnsWrap']//table/tbody[@class='columnSetting singleLine']/tr[5]/td[1]/div/div[1]"
+
+    #Eight Question
+    eight_question_pattern="//ul[@class='add-q-menu-right']//a[text()='Multiple Textboxes']"
+    eight_question_1 = "//table[@id='rows']/tbody[@class='answerSettingTextboxes']/tr[3]/td[1]/div/div[1]"
+    eight_question_2 = "//table[@id='rows']/tbody[@class='answerSettingTextboxes']/tr[4]/td[1]/div/div[1]"
+    eight_question_3 = "//table[@id='rows']/tbody[@class='answerSettingTextboxes']/tr[5]/td[1]/div/div[1]"
+    eight_question_4 = "//table[@id='rows']/tbody[@class='answerSettingTextboxes']/tr[6]/td[1]/div/div[1]"
+    eight_question_5 = "//table[@id='rows']/tbody[@class='answerSettingTextboxes']/tr[7]/td[1]/div/div[1]"
 
     # Ten Question
     ten_question_pattern = "//ul[@class='add-q-menu-left']//a[text()='Comment Box']"
@@ -133,6 +150,7 @@ class AskQuestion(BasePage):
         AskQuestion.next_question(self)
 
     def six_question(self, six_question_enter=""):
+        time.sleep(5)
         self.send_keys(six_question_enter, self._enter_question)
         AskQuestion.enter_question_type(self)
         self.element_click(self.six_question_pattern, locator_type="xpath")
@@ -141,11 +159,47 @@ class AskQuestion(BasePage):
         AskQuestion.next_question(self)
 
     def six_question_special_type(self):
-        self.send_keys(six_1, self.six_question_1,locator_type="xpath")
+        self.send_keys(six_1, self.six_question_1, locator_type="xpath")
         self.send_keys(six_2, self.six_question_2, locator_type="xpath")
         self.send_keys(six_3, self.six_question_3, locator_type="xpath")
         self.send_keys(six_4, self.six_question_4, locator_type="xpath")
         self.send_keys(six_5, self.six_question_5, locator_type="xpath")
+
+    def seven_question(self, seven_question_enter=""):
+        time.sleep(5)
+        self.send_keys(seven_question_enter, self._enter_question)
+        AskQuestion.enter_question_type(self)
+        self.wait_for_element(locator=self.seven_question_pattern, locator_type="xpath", pollFrequency=1)
+        self.element_click(self.seven_question_pattern, locator_type="xpath")
+        AskQuestion.seven_question_special_type(self)
+        AskQuestion.save_button(self)
+        AskQuestion.next_question(self)
+
+    def seven_question_special_type(self):
+        self.send_keys(seven_1, self.seven_question_1, locator_type="xpath")
+        self.send_keys(seven_2, self.seven_question_2, locator_type="xpath")
+        self.send_keys(seven_3, self.seven_question_3, locator_type="xpath")
+        self.send_keys(seven_4, self.seven_question_4, locator_type="xpath")
+        self.send_keys(seven_5, self.seven_question_5, locator_type="xpath")
+        self.send_keys(seven_6, self.seven_question_6, locator_type="xpath")
+        self.send_keys(seven_7, self.seven_question_7, locator_type="xpath")
+
+    def eight_question(self, eight_question_enter=""):
+        time.sleep(5)
+        self.send_keys(eight_question_enter, self._enter_question)
+        AskQuestion.enter_question_type(self)
+        self.wait_for_element(locator=self.eight_question_pattern, locator_type="xpath", pollFrequency=1)
+        self.element_click(self.eight_question_pattern, locator_type="xpath")
+        AskQuestion.eight_question_special_type(self)
+        AskQuestion.save_button(self)
+        AskQuestion.next_question(self)
+
+    def eight_question_special_type(self):
+        self.send_keys(six_1, self.eight_question_1, locator_type="xpath")
+        self.send_keys(six_2, self.eight_question_2, locator_type="xpath")
+        self.send_keys(six_3, self.eight_question_3, locator_type="xpath")
+        self.send_keys(six_4, self.eight_question_4, locator_type="xpath")
+        self.send_keys(six_5, self.eight_question_5, locator_type="xpath")
 
     def verify_question(self):
         self.wait_for_element(locator=self._survey_body, timeout=5, pollFrequency=1)
