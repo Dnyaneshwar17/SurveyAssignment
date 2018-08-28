39ac7d96a84006a976e1d8196aa19722d2987e65
Dnyaneshwar17 <42296046+Dnyaneshwar17@users.noreply.github.com>
2018-08-28 19:52:36 +0530

    Done change in variable name

--- testcasedemo/test_login.py
+++ testcasedemo/test_login.py
@@ -5,19 +5,18 @@ import utilities_config.custom_logger as cl
 from utilities_config.Input import *
 import logging
 
-@pytest.mark.usefixtures("get_driver")
-
+@pytest.mark.usefixtures("one_time_browser_setup")
 class TestLoginDemo(unittest.TestCase):
     log = cl.customLogger(logging.DEBUG)
 
     @pytest.fixture(autouse=True)
-    def objectSetup(self, select_browser):
-        self.log = Login(select_browser)
+    def objectSetup(self, one_time_browser_setup):
+        self.login_start = Login(one_time_browser_setup)
 
 
     @pytest.mark.run(order=2)
     def test_validLogin(self):
         print("test_validLogin started")
-        self.log.startlogin(username, password)
-        result = self.log.login_successful()
+        self.login_start.startlogin(username, password)
+        result = self.login_start.login_successful()
         assert result == True
