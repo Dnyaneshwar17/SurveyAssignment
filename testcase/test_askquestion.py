f4369da44515ef8ec81fced1adec272a6988a7b3
Dnyaneshwar17 <42296046+Dnyaneshwar17@users.noreply.github.com>
2018-08-29 19:19:32 +0530

    added question test file

---
+++ testcase/test_askquestion.py
@@ -0,0 +1,23 @@
+from pages.AskQuestion import AskQuestion
+import unittest
+import pytest
+import utilities_config.custom_logger as cl
+from utilities_config.Input import*
+import logging
+
+@pytest.mark.usefixtures("start_survey")
+class TestAskquestionSurvey(unittest.TestCase):
+    log = cl.customLogger(logging.DEBUG)
+
+    @pytest.fixture(autouse=True)
+    def objectSetup(self, start_survey):
+        self.question = AskQuestion(start_survey)
+
+    @pytest.mark.run(order=4)
+    def test_askquestion(self):
+        print("start question on survey")
+        self.question.first_question(first_question_enter)
+        result = self.question.verify_question()
+        assert result == True
+
+    
