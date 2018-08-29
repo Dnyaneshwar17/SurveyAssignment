d80775f9de66326baafaa71764275936bb9f40a3
Dnyaneshwar17 <42296046+Dnyaneshwar17@users.noreply.github.com>
2018-08-29 19:44:03 +0530

    just change in web driver

--- testcase/test_create_survey.py
+++ testcase/test_create_survey.py
@@ -5,15 +5,15 @@ import utilities_config.custom_logger as cl
 from utilities_config.Input import *
 import logging
 
-@pytest.mark.usefixtures("one_time_browser_setup")
+@pytest.mark.usefixtures("start_survey")
 class TestCreateSurvey(unittest.TestCase):
     log = cl.customLogger(logging.DEBUG)
 
     @pytest.fixture(autouse=True)
-    def objectSetup(self, one_time_browser_setup):
-        self.sc = CreateSurvey(one_time_browser_setup)
+    def objectSetup(self, start_survey):
+        self.sc = CreateSurvey(start_survey)
 
-    @pytest.mark.run(order=1)
+    @pytest.mark.run(order=2)
     def test_create_survey(self):
         print("start create survey")
         self.sc.start_create_survey(first_survey_name)
--- testcase/test_login.py
+++ testcase/test_login.py
@@ -13,10 +13,17 @@ class TestLoginDemo(unittest.TestCase):
     def objectSetup(self, one_time_browser_setup):
         self.login_start = Login(one_time_browser_setup)
 
+    # @pytest.mark.run(order=1)
+    # def test_Failed_Valid(self):
+    #     print("InvalidLogin")
+    #     self.login_start.startlogin(usernamefaili, password)
+    #     result = self.login_start.login_failed()
+    #     assert result == True
 
-    @pytest.mark.run(order=2)
-    def test_validLogin(self):
-        print("test_validLogin started")
+    @pytest.mark.run(order=1)
+    def test_Login_Valid(self):
+        print("valid login started")
         self.login_start.startlogin(username, password)
         result = self.login_start.login_successful()
         assert result == True
+
