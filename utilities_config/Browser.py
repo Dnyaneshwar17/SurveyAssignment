74064cecfc95b030eb1177f436c8f2c6714ba51c
Dnyaneshwar17 <42296046+Dnyaneshwar17@users.noreply.github.com>
2018-08-29 19:25:07 +0530

    commit the code

--- utilities_config/Browser.py
+++ utilities_config/Browser.py
@@ -1,42 +1,42 @@
-from selenium import webdriver
-from utilities_config.Input import InputParameter
-import os
-
-
-class BrowserConfig(object):
-
-      def chrome(self):
-          driverLocation = "D:\\Automation\\chromedriver"
-          os.environ["webdriver.chrome.driver"] = driverLocation
-          driver = webdriver.Chrome(driverLocation)
-          driver.get(InputParameter.url)
-          driver.maximize_window()
-          return driver
-
-
-      def firebox(self):
-          driver = webdriver.Firefox()
-          driver.get(InputParameter.url)
-          driver.maximize_window()
-          return driver
-
-
-      def ie(self):
-          driverLocation = "D:\\Automation\\IEDriverServer.exe"
-          os.environ["webdriver.ie.driver"] = driverLocation
-          driver = webdriver.Ie(driverLocation)
-          driver.get(InputParameter.url)
-          driver.maximize_window()
-          return driver
-
-      def select_browser(self):
-          browser = BrowserConfig.browser
-          if browser == "Chrome":
-            return BrowserConfig.chrome()
-          elif browser == "Firefox":
-           return BrowserConfig.firebox()
-          elif browser == "IE":
-           return BrowserConfig.ie(self)
-          else:
-           print("Please Select Proper Browser")
-           return 0
+# from selenium import webdriver
+# from utilities_config.Input import InputParameter
+# import os
+#
+#
+# class BrowserConfig(object):
+#
+#       def chrome(self):
+#           driverLocation = "D:\\Automation\\chromedriver"
+#           os.environ["webdriver.chrome.driver"] = driverLocation
+#           driver = webdriver.Chrome(driverLocation)
+#           driver.get(InputParameter.url)
+#           driver.maximize_window()
+#           return driver
+#
+#
+#       def firebox(self):
+#           driver = webdriver.Firefox()
+#           driver.get(InputParameter.url)
+#           driver.maximize_window()
+#           return driver
+#
+#
+#       def ie(self):
+#           driverLocation = "D:\\Automation\\IEDriverServer.exe"
+#           os.environ["webdriver.ie.driver"] = driverLocation
+#           driver = webdriver.Ie(driverLocation)
+#           driver.get(InputParameter.url)
+#           driver.maximize_window()
+#           return driver
+#
+#       def select_browser(self):
+#           browser = BrowserConfig.browser
+#           if browser == "Chrome":
+#             return BrowserConfig.chrome()
+#           elif browser == "Firefox":
+#            return BrowserConfig.firebox()
+#           elif browser == "IE":
+#            return BrowserConfig.ie(self)
+#           else:
+#            print("Please Select Proper Browser")
+#            return 0
