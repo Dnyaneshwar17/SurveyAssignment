9273fa47c991a7d2af9a1d283ae6536b92b0b41f
Dnyaneshwar17 <42296046+Dnyaneshwar17@users.noreply.github.com>
2018-09-03 20:11:47 +0530

    rename user name and password

--- pages/Login.py
+++ pages/Login.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
-from  base.Driver_method import SeleniumDriver
-from base.Basepage import BasePage
+from  base.selenium_driver import SeleniumDriver
+from base.basepage import BasePage
 
 class Login(BasePage):
 
@@ -11,10 +11,8 @@ class Login(BasePage):
     login = "LOG IN"
     login_username = "username"
     login_password = "password"
-    usernamet= "mauli123"
-    passwordt = "m@uli123"
-    _login_success="userAcctTab_MainMenu"
-    _login_failed="//div[@id='sign-in']//li[contains(text(),'The username or password you entered is incorrect')]"
+    _login_success = "userAcctTab_MainMenu"
+    _login_failed = "//div[@id='sign-in']//li[contains(text(),'The username or password you entered is incorrect')]"
     _login_submit = "//form[@id='sign_in_form']//button[@type='submit']"
 
     def login_click(self):
@@ -30,17 +28,15 @@ class Login(BasePage):
         self.element_click(self._login_submit, locator_type="xpath")
 
     def login_successful(self):
-        self.wait_for_element(locator=self._login_success, timeout=5,pollFrequency=1)
+        self.wait_for_element(locator=self._login_success, timeout=5, pollFrequency=1)
         result = self.is_element_present(locator=self._login_success)
         return result
 
     def login_failed(self):
-        result = self.is_element_present(locator=self._login_fail,locator_type="xpath")
+        result = self.is_element_present(locator=self._login_failed, locator_type="xpath")
         return result
 
-
-
-    def startlogin(self, email="", password=" "):
+    def startlogin(self, email="", password=""):
         Login.login_click(self)
         Login.email(self, email)
         Login.password(self, password)
